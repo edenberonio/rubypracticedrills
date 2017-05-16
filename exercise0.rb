b75772caa42a96aea54feac82d1ded343d2be1da
Eden Beronio <edenberonio@gmail.com>
2017-05-16 14:53:57 -0400

    First commit

---
+++ exercise0.rb
@@ -0,0 +1,9 @@
+#Save each of the following arrays and hashes into variables. You will use them throughout the assignment
+
+#Create an array that contains the given information:
+
+#your favourite colours
+#the age of you and your siblings/cousins/friends
+#flip a coin 5 times and record whether or not the result was 'heads'
+#your three favourite performing artists
+#your favourite colours again (this time as symbols instead of strings)
